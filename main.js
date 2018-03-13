b7a77c04f2cdaf18a65b6bdd5f32e7212b59df63
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-13 14:00:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -189,23 +189,3 @@ if(coins >= dblKillCost){                                   //checks that the pl
     var nextCost = Math.floor(100 * Math.pow(1.1,dblKills));       //works out the cost of the next cursor
     document.getElementById('dblKillCost').innerHTML = nextCost;  //updates the cursor cost for the user	
 };
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
